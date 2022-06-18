9c1d82f57f464fd592e6e9fafe9a8c2cba38bb6f
Your Name <you@example.com>
2022-06-18 10:43:08 -0700

    u

--- pviclient/index.cjs
+++ pviclient/index.cjs
@@ -149,7 +149,7 @@ class ClientWeb extends Client {
     jwt_parse(token) {
         var base64Url = token.split('.')[1];
         var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
         return JSON.parse(jsonPayload);
