e5a6c67c47bd8b2bf1296396ff9d7aafaf1e8633
Your Name <you@example.com>
2022-06-24 14:13:53 -0700

    u

--- pvicommon/index.cjs
+++ pvicommon/index.cjs
@@ -339,7 +339,7 @@ class TemplateElem {
     forwardToServer(messageIn) {
         let messageOut = {
             UseCaseElemName: this.useCaseElem.spec.Name,
-            ItemId: this.item.id,
+            ItemId: this.item != null ? this.item.id : '1',
             ...messageIn
         };
         this.parent.forwardToServer(messageOut);
