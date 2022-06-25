3ae01914f819493411cfd561c3c5f61e866ad959
Your Name <you@example.com>
2022-06-24 21:37:22 -0700

    u

--- pvicommon/index.cjs
+++ pvicommon/index.cjs
@@ -382,7 +382,7 @@ class TemplateElem {
     constructor(parent, useCaseElem) {
         this.parent = parent;
         this.useCaseElem = useCaseElem;
-        if (this.useCaseElem.spec.Join != null && (this.useCaseElem.spec.Join == 'Yes') {
+        if (this.useCaseElem.spec.Join != null && this.useCaseElem.spec.Join == 'Yes') {
             this.fJoin = true;
         } else {
             this.fJoin = false;
