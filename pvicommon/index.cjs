5dde8629f0512013362e681d47ebe15977661d37
Your Name <you@example.com>
2022-06-26 14:47:56 -0700

    u

--- pvicommon/index.cjs
+++ pvicommon/index.cjs
@@ -644,7 +644,13 @@ class TrackServer extends Track {
     getInitialMessage(){
         return({
             UseCaseSpec: this.template.useCase.spec,
-            ItemSpec: this.template.item
+            ItemSpec: {
+                DBId: this.template.item.dbId,
+                Id: this.template.item.id,
+                Ext: this.template.item.ext,
+                Attrs: this.template.item.attrs,
+                ChildItems: this.template.item.childItems
+            }
         });
     }
 
